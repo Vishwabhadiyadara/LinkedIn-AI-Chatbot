2ed67478cf1179eb74f5f9251b9339a60c459d83
Vishwa Bhadiyadara <110348340+Vishwabhadiyadara@users.noreply.github.com>
2024-10-10 12:57:06 -0400

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,86 @@
+import os
+from dotenv import dotenv_values
+import streamlit as st
+from groq import Groq
+
+
+def parse_groq_stream(stream):
+    for chunk in stream:
+        if chunk.choices:
+            if chunk.choices[0].delta.content is not None:
+                yield chunk.choices[0].delta.content
+
+
+# streamlit page configuration
+st.set_page_config(
+    page_title="Grapvine",
+    page_icon="🍇",
+    layout="centered",
+)
+
+
+try:
+    secrets = dotenv_values(".env")  # for dev env
+    GROQ_API_KEY = secrets["GROQ_API_KEY"]
+except:
+    secrets = st.secrets  # for streamlit deployment
+    GROQ_API_KEY = secrets["GROQ_API_KEY"]
+
+# save the api_key to environment variable
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+INITIAL_RESPONSE = secrets["INITIAL_RESPONSE"]
+INITIAL_MSG = secrets["INITIAL_MSG"]
+CHAT_CONTEXT = secrets["CHAT_CONTEXT"]
+
+
+client = Groq()
+
+# initialize the chat history if present as streamlit session
+if "chat_history" not in st.session_state:
+    # print("message not in chat session")
+    st.session_state.chat_history = [
+        {"role": "linkedln assistant",
+         "content": INITIAL_RESPONSE
+         },
+    ]
+
+# page title
+st.title("Hi I am your linkedln assistant🍇!")
+st.caption("I will analyze your profile ")
+# the messages in chat_history will be stored as {"role":"user/assistant", "content":"msg}
+# display chat history
+for message in st.session_state.chat_history:
+    # print("message in chat session")
+    with st.chat_message("role", avatar='🍇'):
+        st.markdown(message["content"])
+
+
+# user input field
+user_prompt = st.chat_input("Hello i am Linkedln Assistant please ask me a question?")
+
+if user_prompt:
+    # st.chat_message("user").markdown
+    with st.chat_message("user", avatar="🍇"):
+        st.markdown(user_prompt)
+    st.session_state.chat_history.append(
+        {"role": "user", "content": user_prompt})
+
+    # get a response from the LLM
+    messages = [
+        {"role": "system", "content": CHAT_CONTEXT
+         },
+        {"role": "Linkedln assistant", "content": INITIAL_MSG},
+        *st.session_state.chat_history
+    ]
+
+    # Display assistant response in chat message container
+    with st.chat_message("Linkedln Asistant", avatar='🍇'):
+        stream = client.chat.completions.create(
+            model="llama3-8b-8192",
+            messages=messages,
+            stream=True  # for streaming the message
+        )
+        response = st.write_stream(parse_groq_stream(stream))
+    st.session_state.chat_history.append(
+        {"role": "assistant", "content": response})
